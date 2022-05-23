9042c7a9489d3c6de0b48868c36bc9ef01cc8b6f
wabbas-morpheus <102543352+wabbas-morpheus@users.noreply.github.com>
2022-05-23 18:54:20 +0100

    Update AllUsrCase66162.sh

--- AllUsrCase66162.sh
+++ AllUsrCase66162.sh
@@ -1,7 +1,9 @@
-FILE_PATH="/var/opt/morpheus/morpheus-local/repo/git/fa1eb481e857370c888c18e7bf13536csh"
+FILE_PATH="/var/opt/morpheus/morpheus-local/repo/git/fa1eb481e857370c888c18e7bf13536c"
 DESTINATION_PATH="/var/opt/morpheus/morpheus-ui/tempRepoTest"
 USER_NAMES="Internal"
 FILE_TO_CHANGE="SSH.py"
-MY_PATH="$FILE_PATH/$FILE_TO_CHANGE $DESTINATION_PATH/$USER_NAMES/"
-echo $MY_PATH
+MY_PATH1="$FILE_PATH/$FILE_TO_CHANGE"
+MY_PATH2="$DESTINATION_PATH/$USER_NAMES/"
+echo $MY_PATH1
+echo $MY_PATH2
 echo "Hello"
