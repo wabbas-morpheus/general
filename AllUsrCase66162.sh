4ca5d810b04d63ea60b0dc572401af0bcf12bb22
wabbas-morpheus <102543352+wabbas-morpheus@users.noreply.github.com>
2022-05-23 18:05:56 +0100

    Update AllUsrCase66162.sh

--- AllUsrCase66162.sh
+++ AllUsrCase66162.sh
@@ -2,11 +2,14 @@ FILE_PATH="/var/opt/morpheus/morpheus-local/repo/git/fa1eb481e857370c888c18e7bf1
 DESTINATION_PATH="/var/opt/morpheus/morpheus-ui/tempRepoTest"
 
 #Set Customer Names Here 
-#USER_NAMES="Internal"
+USER_NAMES="Internal"
 
 #Set Files to Change Names Here
-#FILE_TO_CHANGE="SSH.py"
+FILE_TO_CHANGE="SSH.py"
+
+cp $FILE_PATH/$FILE_TO_CHANGE $DESTINATION_PATH/$USER_NAMES/
 echo "Hello"
+
 #for i in $USER_NAMES; do
 #    for n in $FILE_TO_CHANGE; do
 #        cp $FILE_PATH/$n $DESTINATION_PATH/$i/
