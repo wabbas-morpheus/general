5c5272c1627c63cf49f6ddffea537a2691c0d1f1
wabbas-morpheus <102543352+wabbas-morpheus@users.noreply.github.com>
2022-12-14 17:59:00 +0000

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+variable "subId" {
+  type    = string
+  default = ""
+  description = "Enter the subscription_id you receive per mail."
+}
+variable "clientId" {
+  type    = string
+  default = ""
+  description = "Enter the client_id you receive per mail."
+}
+
+variable "tenantId" {
+  type    = string
+  default = ""
+  description = "Enter the tenant_id you receive per mail."
+}
+
+variable "clientSecret" {
+  type    = string
+  default = ""
+  description = "Enter the client_secret you receive per mail."
+}
+
+variable "resgrp" {
+  type    = string
+  default = "MyRG001"
+  description = "Enter the resourcegroup you receive per mail."
+}
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  required_version =">= 1.0.1"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+subscription_id =var.subId
+tenant_id=var.tenantId
+client_id = var.clientId
+client_secret = var.clientSecret
+skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "my_rg" {
+  name     = var.resgrp
+  location = "uksouth"
+}
